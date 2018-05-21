9e8e4490d1e48cec7c36a401d959f26a28b83792
zxj886 <496580892@qq.com>
2018-05-21 16:29:41 +0800

    mapper动态代理

--- BaseMapperMybatis/src/main/java/com/zx/util/SessionFactoryUtil.java
+++ BaseMapperMybatis/src/main/java/com/zx/util/SessionFactoryUtil.java
@@ -1,10 +1,12 @@
 package com.zx.util;
 
 
+
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
 
 import java.io.IOException;
 import java.io.InputStream;
